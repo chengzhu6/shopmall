cd46afef819e3dbea084944f2b93196505b1cea3
Castellan <1061541395@qq.com>
2019-01-03 18:40:34 +0800

    generator commit

---
+++ src/main/java/com/study/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.study.dao;
+
+import com.study.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
